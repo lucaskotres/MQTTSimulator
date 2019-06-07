73b0cf3d73524c4ef87fd301a3f7f8dc9ec3fe0a
Lucas Kotres <lucaskotres@gmail.com>
2019-06-06 23:57:09 -0300

    update simulator.py

--- client/client.py
+++ client/client.py
@@ -7,6 +7,7 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     print(str(datetime.datetime.now())+" - "+msg.topic+" -  "+str(msg.payload))
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,8 +1,11 @@
 import models  
+from time import sleep
 
 motor1 = models.motor(True, rotation = 15, minimum = 10, maximum = 30)
 pump1 = models.pump(True, 60)
 
-motor1.simulate_rotation()
-motor1.publish_state()
-motor1.publish_rotation()
+while True:
+    motor1.simulate_rotation()
+    motor1.publish_state()
+    motor1.publish_rotation()
+    sleep(2)
